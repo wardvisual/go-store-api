17946acb2f9110115a883b3cb2bc697d999035d3
wardvisual <wardvisual@gmail.com>
2024-07-30 15:50:07 +0800

    chore(types): add common types

---
+++ types/types.go
@@ -0,0 +1,23 @@
+package types
+
+type User struct {
+	ID        int    `json: "id"`
+	FirstName string `json: "firstName"`
+	LastName  string `json: "lastName"`
+	Email     string `json: "email"`
+	Password  string `json: "password"`
+	CreatedAt string `json: "createdAt"`
+}
+
+type UserStore interface {
+	FindByEmail(email string) (*User, error)
+	FindById(id int) (*User, error)
+	Create(User) error
+}
+
+type RegisterUserPayload struct {
+	FirstName string `json: "firstName" validate: "required"`
+	LastName  string `json: "lastName" validate: "required"`
+	Email     string `json: "email" validate: "required"`
+	Password  string `json: "password" validate: "required,min=3,max=130"`
+}
