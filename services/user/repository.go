8e2256fe26ccacb04db664aa61291d9069aa13d0
wardvisual <wardvisual@gmail.com>
2024-07-30 15:38:26 +0800

    feat(service): add user repository

---
+++ services/user/repository.go
@@ -0,0 +1,26 @@
+package user
+
+import (
+	"database/sql"
+
+	"github.com/wardvisual/go-store-api/types"
+)
+
+type Repository struct {
+	db *sql.DB
+}
+
+func NewStore(db *sql.DB) *Repository {
+	return &Repository{db}
+}
+
+func (r *Repository) CreateUser(user types.User) error {
+	var statement string = "INSERT INTO users (firstName, lastName, email, password), VALUES(?,?,?,?)"
+	_, err := r.db.Exec(statement, user.FirstName, user.LastName, user.Email, user.Password)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
